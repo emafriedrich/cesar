96c6880a9ca4c1d4a20ae6bb2572097d42dfef40
Emanuel Friedrich <aemanuelfriedrich@gmail.com>
2015-07-03 14:37:31 -0300

    improve tests

--- tests/cesar_test.rb
+++ tests/cesar_test.rb
@@ -1,5 +1,16 @@
 require File.expand_path("../lib/cesar", File.dirname(__FILE__))
 
+def assert_exist_file export_path
+  begin
+    assert File.exists?(export_path)
+    assert File.size(export_path) != 0
+    File.delete export_path  
+    true
+  rescue
+    false
+  end
+end
+
 scope do 
 
   test "non nil or empty report_path" do 
@@ -11,18 +22,18 @@ scope do
     end
   end
 
-  test "pdf export" do 
+  test "pdf export with generate method" do 
     export_path =   File.expand_path('andalacosa.pdf')
     Cesar.generate  File.expand_path('test'), 
                     child_node: 'COMPROBANTE',
                     datasource: :xml,
+                    export_to: :pdf,
                     export_path: export_path,
                     datasource_path: File.expand_path('test.xml')
-    
-    assert File.exists?(export_path)
+    assert_exist_file export_path
   end
 
-  test "xls export" do 
+  test "xls export with generate method" do 
     export_path = File.expand_path('andalacosa.xls')
     Cesar.generate  File.expand_path('test'), 
                     child_node: 'COMPROBANTE',
@@ -31,6 +42,43 @@ scope do
                     export_path: export_path,
                     datasource_path: File.expand_path('test.xml')
     
-    assert File.exists?(export_path)
+    assert_exist_file export_path
+  end
+
+  test "xls export with excel_export method" do 
+    export_path = File.expand_path('andalacosa.xls')
+    Cesar.excel_export  File.expand_path('test'), 
+                        child_node: 'COMPROBANTE',
+                        datasource: :xml,
+                        export_path: export_path,
+                        datasource_path: File.expand_path('test.xml')
+    
+    assert_exist_file export_path
+  end
+
+  test "invalid datasource option" do
+    export_path = File.expand_path('andalacosa.xls')
+    assert_raise ArgumentError do 
+      Cesar.excel_export  File.expand_path('test'), 
+                        datasource: :bad,
+                        datasource_path: File.expand_path('test.xml')
+    end
+  end
+
+  test "empty datasource_path" do 
+    export_path = File.expand_path('andalacosa.xls')
+    assert_raise ArgumentError do 
+      Cesar.excel_export  File.expand_path('test'), 
+                        datasource: :xml,
+                        export_path: export_path
+    end
+  end
+
+  test "empty export_path" do
+    assert_raise ArgumentError do 
+      Cesar.excel_export  File.expand_path('test'), 
+                          datasource: :xml,
+                          datasource_path: File.expand_path('test.xml')
+    end
   end
 end
